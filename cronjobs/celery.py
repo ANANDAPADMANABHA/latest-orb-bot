b4c6609a8b0b4412bf88a3e96f82fa44d8331bed
ananthu <ananthapadmanabhan012@gmail.com>
2025-09-09 22:24:27 +0530

    CRON JOB UPDATED

--- cronjobs/celery.py
+++ cronjobs/celery.py
@@ -21,7 +21,7 @@ app.autodiscover_tasks(['cronjobs'])
 app.conf.beat_schedule = {
     'run-trade-task-every-morning': {
         'task': 'cronjobs.tasks.run_trade_task',
-        'schedule': crontab(minute=10, hour=10, day_of_week='mon-fri'),
+        'schedule': crontab(minute=9, hour=20, day_of_week='mon-fri'),
     },
 }
 
